27e0a7e40b4940d343a8ef40d81c0f8af8aa7906
x4ker <pasha230899@gmail.com>
2020-01-06 10:25:59 +0300

    F

--- include/header.hpp
+++ include/header.hpp
@@ -3,7 +3,32 @@
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
 
-#include <Element.h>
+#include <any>
+
+template <typename T>
+class Element {
+ public:
+  T value;
+  std::any nextEl;
+
+ public:
+  Element() = default;
+  Element(const T& val, Element<T> next) {
+    this->value = val;
+    this->nextEl = next;
+  }
+
+  void reset(Element<T> El) {
+    this->value = El.value;
+    this->nextEl = El.nextEl;
+  }
+
+  const T& getValue() const { return this->value; }
+
+  auto getNext() { return std::any_cast<Element<T>>(this->nextEl); }
+
+  ~Element() = default;
+};
 
 template <typename T>
 class Stack {
